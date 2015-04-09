7e11d15ac58264c8c93af9e4899473f0b14d1906
ColeHud <cmhudson11@gmail.com>
2015-04-09 18:22:54 -0500

    Initial Commit

---
+++ InstaGrub/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  InstaGrub
+//
+//  Created by Cole on 4/9/15.
+//  Copyright (c) 2015 Cole Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ InstaGrub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaGrub
+//
+//  Created by Cole on 4/9/15.
+//  Copyright (c) 2015 Cole Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
